591319803bad3a00640862a4c93bddf0547f715b
Luka Miljak <L.Miljak@student.tudelft.nl>
2020-02-17 10:29:56 +0100

    Move Exampels to seperate file

--- InfixGrammar.v
+++ InfixGrammar.v
@@ -20,78 +20,8 @@ Fixpoint yield {L O} (pt : parse_tree L O) : word L O :=
   | INode pt1 op pt2 => yield pt1 ++ inr op :: yield pt2
   end.
 
-Inductive ex_lex :=
-  | A
-  | B
-  | C.
-
-Inductive ex_op :=
-  | o
-  | p
-  | q.
-
-Example yield_simple_infix :
-  yield (INode (ANode A) o (ANode B)) = [inl A; inr o; inl B].
-Proof. reflexivity. Qed.
-
-Example yield_large_nested_infix :
-  yield (INode (INode (ANode B) p (ANode C)) o (INode (ANode A) q (INode (ANode C) o (ANode B)))) 
-  = [inl B; inr p; inl C; inr o; inl A; inr q; inl C; inr o; inl B].
-Proof. reflexivity. Qed.
-
 Definition language {L O} (w : word L O) : Prop :=
-  exists pt, yield pt = w.
-
-Example simple_infix_in_language :
-  language [inl A; inr o; inl B].
-Proof.
-  exists (INode (ANode A) o (ANode B)).
-  apply yield_simple_infix.
-Qed.
-
-Example large_nested_infix_in_language :
-  language [inl B; inr p; inl C; inr o; inl A; inr q; inl C; inr o; inl B].
-Proof.
-  exists (INode (INode (ANode B) p (ANode C)) o (INode (ANode A) q (INode (ANode C) o (ANode B)))).
-  apply yield_large_nested_infix.
-Qed.
-
-Lemma list_length_two {A} (l1 : list A) l2 l3 e1 e2 e1' e2' :
-  l1 ++ e1 :: l2 ++ e2 :: l3 = [e1'; e2'] ->
-  e1 = e1' /\ e2 = e2'.
-intros.
-  destruct l1; simpl in H.
-  - inv H.
-    destruct l2; simpl in H2.
-    + inv H2.
-      split; reflexivity.
-    + inv H2.
-      destruct l2; discriminate H1.
-  - inv H.
-    destruct l1; simpl in H2.
-    + inv H2.
-      destruct l2; discriminate H1.
-    + inv H2.
-      destruct l1; discriminate H1.
-Qed.
-
-Example lex_op_not_in_language :
-  ~ language [inl A; inr o].
-Proof.
-  intro H.
-  inv H.
-  destruct x; simpl in H0.
-  - discriminate H0.
-  - destruct x1; simpl in H0.
-    + destruct x2; simpl in H0.
-      * discriminate H0.
-      * destruct (yield x2_1); discriminate H0.
-    + rewrite <- app_assoc in H0.
-      simpl in H0.
-      apply list_length_two in H0.
-      destruct H0.
-      discriminate H.
-Qed. 
+  exists pt, yield pt = w. 
 
 Inductive tree_pattern (O : Type) :=
   | NTPatt
@@ -139,7 +69,7 @@ Fixpoint size {L O} (pt : parse_tree L O) : nat :=
   | INode pt1 _ pt2 => S (size pt1 + size pt2)
   end.
 
-Section dgrammar_theorems.
+Section InfixGrammarTheorems.
 Context {L O : Type}.
 Implicit Types l : L.
 Implicit Types g : dgrammar O.
@@ -299,3 +229,5 @@ Proof.
   destruct H0. destruct H1.
   exists x. split; assumption.
 Qed.
+
+End InfixGrammarTheorems.
