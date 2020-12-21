5f7fd762a132398b257a2a48e6973db7c2ee648e
Luka Miljak <github@lukamiljak.com>
2020-12-21 20:42:55 +0100

    Compile.sh clones stdpp repository

--- compile.sh
+++ compile.sh
@@ -1,3 +1,17 @@
+if [ -d stdpp ]
+then
+  echo updating and compiling stdpp library
+else
+  echo stdpp library not detected, cloning git repository
+  git clone https://gitlab.mpi-sws.org/iris/stdpp.git
+  echo finished cloning stdpp
+  echo compiling stdpp library
+fi
+cd stdpp
+git pull
+make
+cd ..
+echo finished compiling stdpp library
 echo compiling MyUtils.v
 coqc MyUtils.v
 echo compiling IGrammar.v
